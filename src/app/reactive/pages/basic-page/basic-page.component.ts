f5962db786729b30f378ef7276a951f4609289f2
Hulford <moraleshulford@gmail.com>
2023-11-26 04:31:39 -0500

    Mostrar errores de validacion en pantalla

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -29,12 +29,16 @@ inStorage:[0, [Validators.required, Validators.min(0)]],
   constructor( private fb: FormBuilder){}
   ngOnInit(): void {
 
-this.myForm.reset( rtx5090);
+//this.myForm.reset( rtx5090);
 }
 
 
   onSave():void{
-    if(this.myForm.invalid)return;
+    if(this.myForm.invalid){
+this.myForm.markAllAsTouched();
+      return;
+    }
+
     console.log(this.myForm.value);
 
     this.myForm.reset({price:0, inStorage:0});
