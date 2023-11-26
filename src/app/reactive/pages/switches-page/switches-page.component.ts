abdfa0f4e3c613e14e0fc00d51c72f4ceb9df023
Hulford <moraleshulford@gmail.com>
2023-11-26 14:33:48 -0500

    Continuacion de proyecto y tarea

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   ]
 })
 export class SwitchesPageComponent {
+public myForm: FormGroup = this.fb.group({
+  gender: ['M', Validators.required],
+  wantNotifications: [true, Validators.required],
+  termsAndConditions: [ true, Validators.requiredTrue],
+})
+
+constructor(private fb: FormBuilder){}
+onSave(){
+  if(this.myForm.invalid) {
+    this.myForm.markAllAsTouched();
+    return;
+  }
+}
+
 
 }
