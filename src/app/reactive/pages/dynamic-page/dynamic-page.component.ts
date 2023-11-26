b0cb11ab560bae64f6a8a4895b60a00bafa7721d
Hulford <moraleshulford@gmail.com>
2023-11-26 14:03:01 -0500

    Validacioes Dinamicas

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -20,12 +20,40 @@ get favoriteGames(){
   return this.myForm.get('favoriteGames') as FormArray;
 }
 
+isValidFiel( field: string ): boolean | null{
+  return this.myForm.controls[field].errors
+  && this.myForm.controls[field].touched;
+
+}
+isValidFieldInArray(formArray: FormArray, i: number){
+  return formArray.controls[i].errors
+  && formArray.controls[i].touched;
+}
+getFieldError(field: string): string | null{
+  if( !this.myForm.controls[field] ) return null;
+const errors = this.myForm.controls[field].errors || {};
+for (const key of Object.keys(errors)) {
+  // console.log(key);
+  switch( key ){
+    case 'required':
+      return 'Este campo es requerido';
+      case 'minlength':
+      return `Minimo ${errors ['minlength'].requiredLenght} caracters.`;
+  }
+
+}
+return null;
+}
+
+
  onSubmit():void{
   if( this.myForm.invalid){
     this.myForm.markAllAsTouched();
 
     return;
   }
+
+
   console.log(this.myForm.value);
   this.myForm.reset();
  }
